6560262d7b466f79ce1b524d2382d309fe753001
Hammad Fazal <75949304+hammad-fazal@users.noreply.github.com>
2025-10-23 17:59:02 +0000

    updates

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "tf-backend-bucket-21102025"
     key    = "state"
     region = "us-east-2"
-    dynamodb_table = "backend" # for state locking 
+    dynamodb_table = "tf-backend" # for state locking 
   }
 }
